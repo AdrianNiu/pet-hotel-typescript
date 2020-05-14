b4670ce784d73ca6f26ba350765d462cf72fd01f
Adrian Niu <niuxx107@umn.edu>
2020-05-14 15:19:03 -0400

    added the nav buttons in top of the page

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Nav, NavItem, NavLink, NavbarText} from 'reactstrap';
+
+const NavBar = (props) => {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Nav>
+        <NavItem>
+          <NavLink href="#/">Dashboard</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#/manage">Mange Owner</NavLink>
+        </NavItem>
+      </Nav>
+    </div>
+  );
+}
+
+export default NavBar;
