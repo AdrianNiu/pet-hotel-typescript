0630d47b45eb977bf92af27eeabdc9aecdd52fd6
Denis Woldemariam <damewold@gmail.com>
2020-05-14 07:49:30 -0500

    added dummyData into the database

--- pet_hotel.sql
+++ pet_hotel.sql
@@ -11,3 +11,15 @@ CREATE TABLE "pet" (
   "pet_breed" VARCHAR (80),
   "check_in" BOOLEAN
 );
+
+
+INSERT INTO "user" ("username", "password")
+VALUES ('alsisonsmith', 'alisonsmith'),
+('danmariao', 'danmariao');
+
+INSERT INTO "pet" ("pet_name", "pet_color", "pet_breed", "check_in")
+VALUES ('Beau', 'Grey', 'Siberian Husky', 'true'),
+('Yoda', 'Black', 'Dobermann', 'true'),
+('Ziggy', 'White', 'Samoyed', 'false'),
+('Zeus', 'Brown', 'GreyHound', 'true'),
+('Milo', 'Black', 'Pug', 'false');
