b9c4828a736d9d845138ee03d91ae64ee2a90b65
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-05-14 15:03:23 -0500

    checkin/checkout functionality

--- src/components/PetTable/PetTable.js
+++ src/components/PetTable/PetTable.js
@@ -7,59 +7,75 @@ import './PetTable.css';
 import { Table } from 'reactstrap';
 
 class PetTable extends Component {
+  componentDidMount() {
+    this.props.dispatch({ type: "GET_PETS" });
+  }
 
-    componentDidMount() {
-        this.props.dispatch({ type: 'GET_PETS' });
-    }
+  deleteEntry = (id) => {
+    console.log("In deleteEntry", id);
+    this.props.dispatch({ type: "DELETE_ENTRY", payload: { pet_id: id } });
+  };
 
-    deleteEntry = (id) => {
-        console.log('In deleteEntry', id);
-        this.props.dispatch({ type: 'DELETE_ENTRY', payload: {pet_id: id}});
-    }
+  checkIn = (event) => {
+    console.log("In checkIn", event);
+    this.props.dispatch({ type: 'CHECKIN_PET', payload: event})
+  };
 
-    checkIn = (event) => {
-    console.log('In checkIn');
-    }
-    
+  checkOut = (event) => {
+    console.log("In checkOut", event);
+    this.props.dispatch({ type: 'CHECKOUT_PET', payload: event})
+  };
 
+  render() {
+    return (
+      <div>
+        <h1>History</h1>
+        <Table>
+          <thead>
+            <tr>
+              <th>Owner</th>
+              <th>Pet</th>
+              <th>Breed</th>
+              <th>Color</th>
+              <th>Checked in</th>
+              <th>Actions</th>
+              <th></th>
+            </tr>
+          </thead>
 
+          <tbody>
+            {this.props.store.petReducer.map((pet) => (
+              <tr key={pet.id}>
+                <td></td>
+                <td>{pet.pet_name}</td>
+                <td>{pet.pet_breed}</td>
+                <td>{pet.pet_color}</td>
+                {pet.check_in ? <td>Yes</td> : <td>No</td>}
+                <td>
+                  <button type="submit" onClick={(event) => this.checkIn(pet.id)}>
+                    Check-In
+                  </button>
 
-    render() {
-        return (
-           
-            <div>
-                <h1>History</h1>
-                <Table> 
-                    <thead>
-                        <tr>
-                            <th>Owner</th>
-                            <th>Pet</th>
-                            <th>Breed</th>
-                            <th>Color</th>
-                            <th>Checked in</th>
-                            <th></th>
-                        </tr>
-                    </thead>
+                  <div className="divider" />
+                  <button type="button" onClick={() => this.checkOut(pet.id)}>
+                    Check-Out
+                  </button>
 
-                    <tbody>
-                        
-                        {this.props.store.petReducer.map(pet => <tr key={pet.id}>
-                        <td></td>
-                        <td>{pet.pet_name}</td>
-                        <td>{pet.pet_breed}</td>
-                        <td>{pet.pet_color}</td>
-                        {pet.check_in ? 
-                        <td>Yes</td>
-                        :
-                        <td>No</td>
-                        }
-                        <td><button type="button" onClick={(event) => this.checkIn(pet)}>Check-In</button><div className="divider"/><button type="button" onClick={() => this.deleteEntry(pet.id)}>Delete</button></td>
-                        </tr>)}
-                    </tbody>
-                </Table> 
-            </div>
-        )
-    }
+                  <div className="divider" />
+                  <button
+                    type="button"
+                    onClick={() => this.deleteEntry(pet.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStoreToProps)(PetTable);
--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,11 +2,15 @@ import { all } from 'redux-saga/effects';
 import ownerSaga from './owner.saga';
 import addPetSaga from './add.pet.saga';
 import getPetSaga from './get.pet.saga';
+import checkinPetSaga from './checkin.pet.saga';
+import checkoutPetSaga from './checkout.pet.saga';
 
 export default function* rootSaga() {
   yield all([
     ownerSaga(),
     addPetSaga(),
     getPetSaga(),
+    checkinPetSaga(),
+    checkoutPetSaga(),
   ]);
 }
